0a21900d32a65b18c2c489fef89226cc81286ebf
minker223 <bulpeter@cassiaschools.org>
2025-11-20 13:35:38 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,43 +1,23 @@
 #!/bin/bash
-# Docker-friendly Eaglercraft Server Launcher
-
 set -e
 
-# Stop any DISPLAY (headless)
-unset DISPLAY
-
-echo "[INFO] Starting Eaglercraft Server in Docker..."
-
-# Start Caddy in the background but keep logs to stdout
-echo "[INFO] Starting Caddy..."
-cd ./Caddy
-caddy stop || true
-caddy start --config ./Caddyfile &
-CADDY_PID=$!
-cd ..
-
-# Start Cuberite server in the foreground
 echo "[INFO] Starting Cuberite..."
 cd ./Cuberite
-chmod +x Cuberite
+chmod +x ./Cuberite
 ./Cuberite &
 CUBERITE_PID=$!
 cd ..
 
-# Start Bungee/Waterfall in the foreground
+# Give Cuberite some time to start
+sleep 5
+
 echo "[INFO] Starting Bungee..."
 cd ./Bungee
 java -Xmx128M -Xms128M -jar bungee.jar &
 BUNGEE_PID=$!
 cd ..
 
-# Function to shut down everything on container stop
-function cleanup {
-    echo "[INFO] Shutting down servers..."
-    kill $BUNGEE_PID $CUBERITE_PID $CADDY_PID || true
-    wait
-}
-trap cleanup SIGTERM SIGINT
+# Trap signals to stop both
+trap "kill $CUBERITE_PID $BUNGEE_PID; wait" SIGINT SIGTERM
 
-# Wait for all background processes
 wait
